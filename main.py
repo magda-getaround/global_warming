300bfa81547e5037b5c3f79a18e47e8d98e9a28e
Magda Kowalska <magda.kowalska@getaround.com>
2020-10-17 23:17:27 -1000

    Cleanin up

--- main.py
+++ main.py
@@ -27,12 +27,14 @@ warnings.simplefilter(action="ignore", category=FutureWarning)
 
 def show_year_avg_graph(df):
     ya = df.groupby(df["YEAR"])["TAVG"].mean().to_frame(name="avg_temp").reset_index()
-    ya.set_index("YEAR")
 
-    temp_df_mean = ya.avg_temp.rolling(window=2).mean()
+    rolling_avg = ya.avg_temp.rolling(window=2).mean()
+    exponential_moving_avg = ya.avg_temp.ewm(span=59).mean()
 
-    temp_df_mean.plot(style="r--", label="Year average temp")
-    ya.avg_temp.ewm(span=59).mean().plot(
+    rolling_avg.plot(
+        style="r--", label="Year average temp"
+    )
+    exponential_moving_avg.plot(
         style="b", label="Exponential moving average"
     )
 
@@ -61,10 +63,12 @@ def show_rainfall_graph(df):
 df = pd.read_csv(
     FILE, parse_dates=["DATE"], sep=",", decimal=".", infer_datetime_format=True
 )
+print(df)
 
 df["TAVG"] = df.apply(
     lambda row: float(statistics.mean([row["TMIN"], row["TMAX"]])), axis=1
 )
+print(df)
 
 df["RAINED"] = df.apply(
     lambda row: 1 if row["PRCP"] > 0 else 0, axis=1
@@ -74,5 +78,5 @@ df["YEAR"] = pd.DatetimeIndex(df["DATE"]).year
 
 # -------------------------------------------------------------------------------------------------------------------- #
 
-show_year_avg_graph(df)
-# show_rainfall_graph(df)
+# show_year_avg_graph(df)
+show_rainfall_graph(df)
